94cbb53a920a9939785343268986b4485c5f9f2f
John Weachock <jweachock@gmail.com>
2019-07-26 16:12:27 -0400

    Update with appropriate CI variable usage

--- mold/build.sh
+++ mold/build.sh
@@ -1,10 +1,10 @@
-if [ -z $CI_COMMIT_REF_NAME ] || [ -z $DOCKER_IMAGE ]; then
-  echo 'Missing image name or commit ref name' >&2
+if [ -z $CI_COMMIT_REF_SLUG ] || [ -z $CI_REGISTRY_IMAGE ]; then
+  echo 'Missing image name or commit ref' >&2
   exit 1
 fi
 
 docker build \
   $DOCKER_ARGS \
   -f ${DOCKERFILE:-Dockerfile} \
-  -t $DOCKER_IMAGE:$CI_COMMIT_REF_NAME \
+  -t "$CI_REGISTRY_IMAGE$IMAGE:$CI_COMMIT_REF_SLUG"
   .
